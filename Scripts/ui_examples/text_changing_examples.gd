eccae1880d0c1c14c9ab2d87115f1274a23dceb7
Mac <macicyengine@gmail.com>
2024-12-29 15:51:56 +0100

    Added counter example

--- Scripts/ui_examples/text_changing_examples.gd
+++ Scripts/ui_examples/text_changing_examples.gd
@@ -1,7 +1,12 @@
 extends Node
 
 @onready var label_example_1: Label = %LabelExample1
+@onready var label_example_2: Label = %LabelExample2
+
+var counter: int = 0
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		label_example_1.text = "Some different text"
+		counter += 1
+		label_example_2.text = str(counter)

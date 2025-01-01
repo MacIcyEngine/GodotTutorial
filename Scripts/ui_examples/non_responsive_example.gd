b589e4a96bcf4ab894eb8c32ccc81c674422d27a
Mac <macicyengine@gmail.com>
2025-01-01 15:33:26 +0100

    Added screen dimensions display to top left corner

--- Scripts/ui_examples/non_responsive_example.gd
+++ Scripts/ui_examples/non_responsive_example.gd
@@ -3,6 +3,8 @@ extends Control
 const WIDTH: int = 1280
 const HEIGHT: int = 720
 
+@onready var resolution: Label = %Resolution
+
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.WHITE)
 	get_tree().root.size.x = HEIGHT
@@ -13,6 +15,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		switch_aspect_ratio()
+
+	resolution.text = str(get_tree().root.size.x, "x", get_tree().root.size.y)
 
 func switch_aspect_ratio() -> void:
 	if get_tree().root.size.x == WIDTH:

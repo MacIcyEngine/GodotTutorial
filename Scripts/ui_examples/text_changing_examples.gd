ccbbdc41570512add30f0a870849cc2c576d08e5
Mac <macicyengine@gmail.com>
2025-01-06 22:31:26 +0100

    Some minor changes

--- Scripts/ui_examples/text_changing_examples.gd
+++ Scripts/ui_examples/text_changing_examples.gd
@@ -1,9 +1,9 @@
-extends Node
+extends Control
 
-@onready var mouse_x_text: Label = %MouseXText
-@onready var mouse_y_text: Label = %MouseYText
 @onready var some_text: Label = %SomeText
 @onready var counter_text: Label = %CounterText
+@onready var mouse_x_text: Label = %MouseXText
+@onready var mouse_y_text: Label = %MouseYText
 
 var counter: int = 0
 
@@ -11,12 +11,12 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		some_text.text = "Some different text"
 
-	if Input.is_action_just_pressed("left_mouse_button"):
+	if Input.is_action_just_pressed("ui_up"):
 		counter += 1
 		counter_text.text = str(counter)
 
 	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
 	mouse_x_text.global_position.x = mouse_pos.x
 	mouse_y_text.global_position.y = mouse_pos.y
-	mouse_x_text.text = str("Mouse x: ", snapped(mouse_pos.x, 1))
-	mouse_y_text.text = str("Mouse y: ", snapped(mouse_pos.y, 1))
+	mouse_x_text.text = str("Mouse x: ", mouse_pos.x)
+	mouse_y_text.text = str("Mouse y: ", mouse_pos.y)
